70caea0287e0958ed0f45705fb201390bc76ac3f
Prabagaran A <74205112+Prabagaran5306@users.noreply.github.com>
2023-02-19 23:02:27 +0530

    Add files via upload

---
+++ src/main/java/com/batmancompany/studentManagement/StudentController.java
@@ -0,0 +1,31 @@
+package com.batmancompany.studentManagement;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class StudentController {
+    Map<Integer,Student> db = new HashMap<>();//database
+
+    @GetMapping("/get_info")
+    Student getStudent(@RequestParam("id") int admnNo){
+        return db.get(admnNo);
+    }
+
+    @PostMapping("/add")
+    String addStudent(@RequestBody() Student student){
+        db.put(student.getAdmnNo(),student);
+        return "Student has been added successfully";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    String deleteStudent(@PathVariable("id") int admnNo){
+        db.remove(admnNo);
+        return "Student deleted";
+    }
+//    Student updateStudentt(int admnNo){
+//
+//    }
+}
